c2fa43f1d32859d2aa64655fbaabba75ac14f9df
JohnChain <johnchain.li@gmail.com>
2019-09-04 21:11:49 +0800

    add names for widgets

---
+++ Makefile
@@ -0,0 +1,7 @@
+UIPREFIX = IOESDemo
+SRC = mainUI.py
+all:
+	pyuic5 $(UIPREFIX).ui -o $(UIPREFIX).py
+	python $(SRC)
+clean:
+	rm -rf __pycache__
--- mainUI.py
+++ mainUI.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 #!/usr/local/bin/python
-from PyQt5 import QtGui, QtWidgets
 import sys
+from PyQt5 import QtGui, QtWidgets
 import IOESDemo
 
 version = "version: 20190904001"
 
-class IOESDemoApp(QtWidgets.QMainWindow, IOESDemo.Ui_MainWindow):
+class IOESDemoApp(QtWidgets.QMainWindow, IOESDemo.Ui_IOESDemo):
     def __init__(self, parent=None):
         super(IOESDemoApp, self).__init__(parent)
         self.setupUi(self)
         self.statusBar().showMessage(version)
+
         self.registEvent()
         self.initStates()
         
@@ -18,7 +19,24 @@ class IOESDemoApp(QtWidgets.QMainWindow, IOESDemo.Ui_MainWindow):
         print("initStates")
 
     def registEvent(self):
-        print("registEvent")
+       self.btnStartTask.clicked.connect(self.startTask)
+       self.btnStopTask.clicked.connect(self.stopTask)
+       self.btnDumpResult.clicked.connect(self.dumpResult)
+       self.btnBraws.clicked.connect(self.brawsImage)
+
+    def startTask(self):
+        print("startTask")
+        #self.gvPreview.
+
+    def stopTask(self):
+        print("stopTask")
+
+    def dumpResult(self):
+        print("dumpResult")
+
+    def brawsImage(self):
+        download_path = QtWidgets.QFileDialog.getExistingDirectory(self, "浏览", "E:\workspace")
+        self.edtImagePath.setText(download_path)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
