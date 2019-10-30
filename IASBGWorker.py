a96154e7f11d0e3e07a1da97bed32276431a97a4
JohnChain <johnchain.li@gmail.com>
2019-10-30 15:09:46 +0800

    avoid crash when starting task with IAS selected

--- IASBGWorker.py
+++ IASBGWorker.py
@@ -1,8 +1,42 @@
 # -*- coding: utf-8 -*-
 #!/usr/local/bin/python
 from BGWorker import *
-from utils import *
+from IOESThread import IOESThread
 
 class IASBGWorker(BGWorker):
     def __init__(self, parent=None):
         super(IASBGWorker, self).__init__()
+        self.threadAddTask = IOESThread()
+        self.freeCounter = 0
+
+    def addTask(self, url, fileList):
+        argc = 2
+        argv = [url, fileList]
+        self.threadAddTask.registFunction(self.addTaskInThead, argc, argv)
+        self.threadAddTask.start()
+
+    def addTaskInThead(self, argc, argv):
+        logger.warning("IAS function not supported yet!!!")
+        #TBD: add task to taskList
+        pass
+
+    def run(self):
+        if(self.flagRun):
+            logger.info("already runing")
+            return
+        self.flagRun = True
+        sigMsg = ""
+        while(self.flagRun):
+            if(len(self.taskList) > 0):
+                self.freeCounter = 0
+                taskJson = self.taskList.pop(0)
+                #TBD: process taskJson
+            else:
+                self.freeCounter += 1
+                if self.freeCounter == 3:
+                    break
+                sleep(1)
+        self.callback(SIG_TYPE_END, sigMsg)
+        self.freeCounter = 0
+        self.flagRun = False
+        logger.info("Thread exited")
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 APP_NAME = "KeenSenseDemo"
-VERSION = "2.2.4_2019102919"
+VERSION = "2.2.4_2019103015"
 DEFAULT_SERVICE_URL = "http://192.168.1.222:9098/images/recog"
 
 PROJECT_IOES = "IOES"
